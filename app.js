5a0292946e4f64cda1db17824d40fddd25f43d62
ehzawad <ehzawad@gmail.com>
2018-07-04 21:41:14 +0600

    damn strong password, proper session and login and logout.. codebase is now smaller

--- app.js
+++ app.js
@@ -1,107 +1,66 @@
-const express		 	= require('express')
-const bodyParser 	 	= require('body-parser')
-const app 				= express()
-const mongoose 			= require('mongoose')
-const cookieParser 		= require('cookie-parser');
-const session           = require('express-session')
-const port 				= 3000 
-
-var connectedUser = {};
-var connectedUserInfo = {};
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var app = express();
 
 // mongodb connection
-mongoose.connect("mongodb://localhost:27017/gossip")
-const db = mongoose.connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/gossiping", { useMongoClient: true });
+var db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
 
-db.on('error', console.error.bind(console, 'connection error'))
-
-// parsing incoming requests
-// create a middleware for urlencodedbody
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-
-// seerce static filess from /public
-// for examples images or css or materiallite
-app.use(express.static(__dirname + '/public'))
-
-// view engine setup
-app.set('view engine', 'pug')
-app.set('views', __dirname + '/views')
-
-// include routes
-// recall, you wrote those in ./routes/index.js file
-// it's not necessary to give extension of js
-const routes 			= require('./routes/index')
-const users 			= require('./routes/users')
-const authentication 	= require('./routes/Authentication')
-const gossip 			= require('./routes/Gossip')
-
-//use sessions for tracking logins
+// use sessions for tracking logins
 app.use(session({
-  secret: 'daf44saz3dbsuvb3218318hsd',
+  secret: 'let fuck',
   resave: true,
   saveUninitialized: false,
-  cookie: { 
-  	secure: false, 
-  	maxAge:269999999999,
-  	httpOnly: true
-  }
+  store: new MongoStore({
+    mongooseConnection: db
+  })
 }));
 
-app.use('/', routes)
-app.use('/', authentication)
-app.use('/user', users)
-app.use('/gossip', gossip)
-
-// few more middleware to catch errors
-
-// catch 404 and forward to Error Handler
-app.use((req, res, next) => {
-  const err = new Error('File not found !!')
-  err.status = 404
-  next(err)
-})
-
-// recall, middleware is a chain on function which passes responsibility through callbacks
-
-
-// Error Handler
-// define as the last app.use callback
-app.use((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.render('error', { message: err.message, error: {} })
-})
-
-
-
-/// Socket configuration
-var io = require('socket.io').listen(app.listen(port));
-
-
-io.sockets.on('connection', function (socket) {
-	//console.log('CHater application');
-
-   	socket.emit('previousOnline', connectedUserInfo);
-
-
-	socket.on('online', function(data){
-
-		connectedUserInfo[data.userId] = data
-		connectedUser[data.userId] = socket
-		var userdata = {};
-		userdata[data.userId] = data;
-
-		socket.broadcast.emit('online', userdata); 
-	})
-
-    //socket.emit('message', { message: 'welcome to the chat' });
-    socket.on('chat', function (data) {
-        console.log(data.to)
-        if (connectedUser[data.to]) {
-        	connectedUser[data.to].emit('chat', data);
-        }
-        connectedUser[data.from].emit('chat', data);	
-    });
+// make user ID available in templates
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.session.userId;
+  next();
 });
 
-//app.listen(3000, () => console.log('express app listening on port 3000'))
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve static files from /public
+app.use(express.static(__dirname + '/public'));
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// include routes
+var routes = require('./routes/index');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,132 @@
-"use strict"
+var express = require('express');
+var router = express.Router();
+var passwordValidator = require('password-validator');
+var User = require('../models/user');
+var mid = require('../middleware');
 
-const express = require('express')
-const router = express.Router()
+// GET /profile
+router.get('/profile', mid.requiresLogin, function(req, res, next) {
+  User.findById(req.session.userId)
+      .exec(function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render('profile', { title: 'Profile', name: user.name});
+        }
+      });
+});
 
-const user = require('../controllers/UserController')
+// GET /logout
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
 
-/// starts with root then
+// GET /login
+router.get('/login', mid.loggedOut, function(req, res, next) {
+  return res.render('login', { title: 'Log In'});
+});
 
-// GET root route
-router.get('/', (req, res, next) => res.render('index', { title: 'Home'}))
+// POST /login
+router.post('/login', function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      }  else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
+// GET /register
+router.get('/register', mid.loggedOut, function(req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+
+var passwordSchema = new passwordValidator();
+
+passwordSchema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits()                                 // Must have digits
+.has().not().spaces()                           // Should not have spaces
+
+// POST /register
+router.post('/register', function(req, res, next) {
+
+  if (passwordSchema.validate(req.body.password) === false) {
+    throw new Error("Password is not Strong");
+  }
+
+  if (req.body.email &&
+    req.body.name &&
+    req.body.password &&
+    req.body.confirmPassword) {
+
+      // confirm that user typed same password twice
+      if (req.body.password !== req.body.confirmPassword) {
+        var err = new Error('Passwords do not match.');
+        err.status = 400;
+        return next(err);
+      }
+
+      // create object with form input
+      var userData = {
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password
+      };
+
+      // use schema's `create` method to insert document into Mongo
+      User.create(userData, function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          req.session.userId = user._id;
+          return res.redirect('/profile');
+        }
+      });
+
+    } else {
+      var err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    }
+})
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
 
 // GET /about
-router.get('/about', (req, res, next) => res.render('about', { title: 'About'}))
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
 
 // GET /contact
-router.get('/contact', (req, res, next) => res.render('contact', { title: 'Contact'}))
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
 
-
-
-
-module.exports = router
-
-// Make sure all router is exported as module otherwise you can't use it in another file
-// console.log(module.exports)
+module.exports = router;
